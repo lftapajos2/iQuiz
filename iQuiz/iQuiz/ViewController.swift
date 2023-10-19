18bcb699e10c32fe3ae016b4a7c3c97685e14946
llgomes <luis.gomes@foton.la>
2023-10-19 10:18:43 -0300

    Criação do projeto iQuiz

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by foton on 19/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
